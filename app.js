125b253d8381f2784ad7c50e3f7701dc2dc98908
spulit <ljcarrasco@gmail.com>
2018-09-14 14:17:47 +0200

    Commit inicial

---
+++ app.js
@@ -0,0 +1,15 @@
+const argv = require('yargs').argv;
+
+
+const { multiplica } =  require('./multiplicar/multiplicar');
+
+let argv2 = process.argv;
+let base = 'N';
+
+// multiplica(base)
+//     .then(a => console.log(`Ac: ${a}`))
+//     .catch(e => console.log(e));
+console.log ( argv);
+console.log(argv);
+
+
